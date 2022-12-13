328f4eb3990af00e91025b2420006a4ee656575d
Edis MR <edisanthony@gmail.com>
2022-12-13 08:04:55 -0600

    build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch: 'full'},
-  {path: 'splash', component: SplashComponent},
   {path: '**', component: NoPageComponent}
 ];
 
